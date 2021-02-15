2181970956d1a1aecf81619966244851ded09329
„Martin <martindanek4@gmail.com>
2021-02-15 20:42:09 +0100

    plot, picture csv

--- covid_hospitalizace.py
+++ covid_hospitalizace.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from pprint import pprint as pp
+import matplotlib.pyplot as plt
 
 
 def download(url):
@@ -15,30 +16,42 @@ def json_grab(data_set):
 
 def data_frame(data_json):
     df_cumul = pd.DataFrame(data_json['data'])
-    df_cumul['datum'] = pd.to_datetime(df_cumul['datum'])
+    df_cumul.columns = [
+        'date', 'confirmed', 'recovered', 'death', 'test', 'ag_test'
+    ]
+    df_cumul['date'] = pd.to_datetime(df_cumul['date'])
+
+    print(df_cumul['death'].tail())
+
     df = df_cumul.diff(axis=0)
-    df['datum'] = df_cumul['datum']
+    df['date'] = df_cumul['date']
     df.to_csv('covid_cz_diff.csv')
     return df
 
 
 def stat(df):
-    print(df.columns)
-    print(df.dtypes)
     print(df.describe())
+    print(df[(df['confirmed'] > 10000)].count())
 
-# {'datum': '2021-02-14',
-#            'kumulativni_pocet_ag_testu': 2044626,
-#            'kumulativni_pocet_nakazenych': 1090860,
-#            'kumulativni_pocet_testu': 4944403,
-#            'kumulativni_pocet_umrti': 18250,
-#            'kumulativni_pocet_vylecenych': 969154}
+
+def draw_df(df):
+    df['confirmed'] = df['confirmed'].rolling(7).mean()
+
+    df.plot(y="confirmed", color='black')
+    plt.yscale('log')
+    plt.title('Day increment')
+
+    plt.savefig('log_confirmed.png')
+    plt.savefig('log_confirmed.svg', format='svg', dpi=1200)
+
+    plt.show()
 
 
 def main(url):
     data_set = download(url)
     data_json = json_grab(data_set)
     df = data_frame(data_json)
+    draw_df(df)
 
     stat(df)
 
