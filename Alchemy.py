a6da45fc939ee3865f3f86b3c7a4a54c72cf8846
„Martin <martindanek4@gmail.com>
2021-04-05 11:37:11 +0200

    pandas london data prep

---
+++ Alchemy.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy import inspect
+
+
+# user_e = "student"
+# password_e = "p7@vw7MCatmnKjy7"
+# conn_string = f"mysql+pymysql://{user_e}:{password_e}@data.engeto.com/data"
+# engeto_conn = sqlalchemy.create_engine(conn_string, echo=True)
+#
+# countries_e_df = pd.read_sql('countries', engeto_conn, parse_dates=True)
+
+user = "lamikoko.cz.1"
+password = "Zzdenka1"
+conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz1"
+
+lamikoko_conn = sqlalchemy.create_engine(conn_string, echo=True)
+
+# countries_e_df.to_sql('countries_II', lamikoko_conn)
+
+# inspector = inspect(lamikoko_conn)
+# schemas = inspector.get_schema_names()
+#
+# for schema in schemas:
+#     print(f'schema: {schema}')
+#     for table_name in inspector.get_table_names(schema=schema):
+#         print(f'table names: {table_name}')
+#         # for column in inspector.get_columns(table_name, schema=schema):
+#         #     print("Column: %s" % column)
+
+
+countries_df = pd.read_sql(
+    'countries', lamikoko_conn, parse_dates=True)
+# print(countries_df.shape)
+
+# covid_df = pd.read_sql(
+#     'covid19_basic_differences', lamikoko_conn, parse_dates=True)
+
+# ===== WHERE UKOL 2 ====
+# df = pd.read_sql_query("SELECT "
+#                        "country, date, confirmed "
+#                        "FROM covid19_basic_differences "
+#                        "WHERE country = 'Austria' ", lamikoko_conn)
+# print(df.dropna())
+
+# df = covid_df.query("country == 'Austria'")[['date', 'country', 'confirmed']]
+# print(df.dropna())
+
+# cond = covid_df.country == 'Austria'
+# df = covid_df.loc[cond, ['date', 'country', 'confirmed']]
+# print(df.dropna())
+
+
+# df = countries_df[['country', 'currency_name']]
+# cond1 = df.dropna().currency_name.str.contains('Dollar')
+# print(df.dropna()[cond1])
--- London_pandas.py
+++ London_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 living_df = pd.read_csv(
@@ -7,8 +8,26 @@ living_df = pd.read_csv(
 
 print(living_df.head(3))
 
-
+# download tables from wiki with pandas - using lxml modul by default
+# SSL certificate problem - update command.certificate in macOS python dir
 link = 'https://en.wikipedia.org/wiki/London_boroughs'
 tables = pd.read_html(link)
+df = tables[2].iloc[:, :2]
+designations_df = df.copy()
+designations_df.loc[:, 'London borough']\
+    .replace({'Hammersmith[notes 2]': 'Hammersmith and Fulham',
+              'Barking[notes 3]': 'Barking and Dagenham'}, inplace=True)
 
-print(tables)
+designations_df.rename(columns={'London borough': 'Area'}, inplace=True)
+
+# join, merge, concatenate
+# merged_df = living_df.merge(designations_df, how='left')
+#
+# living_df.set_index('Area', inplace=True)
+# designations_df.set_index('Area', inplace=True)
+# joined_df = living_df.join(designations_df)
+
+merged_df = pd.merge(living_df, designations_df,
+                     how='left', left_on='Area', right_on='London borough')\
+    .set_index('Area')
+
